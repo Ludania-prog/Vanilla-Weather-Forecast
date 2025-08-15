34f2ce0f85a95433b8b7fd013687815c566c838a
Ludania-prog <ludania.pris@gmail.com>
2025-08-16 00:29:06 +0100

    Added function to the emojis and the day name

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ function refreshweather(response) {
 
   iconElement.innerHTML = `<img src="${iconUrl}" alt="${response.data.weather[0].description}">`;
 
-  console.log(response.data);
-  console.log(response.data.wind.speed);
-  console.log(response.data.weather[0].description);
-  console.log(response.data.main.humidity);
-
   cityElement.innerHTML = response.data.name;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -28,7 +23,7 @@ function refreshweather(response) {
   speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 
-  getForecast(response.data.city);
+  getForecast(response.data.name);
 }
 
 function formatDate(date) {
@@ -69,37 +64,46 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "990t50744fc2a02348640f8o4b9c6621";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
   axios(apiUrl).then(displayForecast);
-
-  console.log(apiUrl);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 
   let forecastHTML = "";
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div ><img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon" />
+        </div>
         <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
-        <strong>15º</strong>
+        <strong>${Math.round(day.temperature.maximum)}º</strong>
         </div>
-        <div class="weather-forecast-temperature">9º</div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}º</div>
         </div>
         </div>
         `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -110,5 +114,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
-
-displayForecast();
